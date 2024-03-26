2fdccc8256d3d86f22c5b3a02a775035d034b953
YoungKing-Joshua <joshuadeialorse@gmail.com>
2024-03-25 21:33:00 -0700

    0x00. Python - Variable Annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""" A type-annotated function """
+""" Adding type-annotated code"""
 
 
 def add(a: float, b: float) -> float:
     """
-        function add that takes a float a and a float b as arguments and
-        returns their sum as a float
+        This is to add floats as arguments and
+        return their sum also as float
     """
     return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""" A  type-annotated function concat """
+""" Concatenatig type-annotatedcode"""
 
 
 def concat(str1: str, str2: str) -> str:
     """
-        Function concat that takes a string str1 and a string str2 as
-        arguments and returns a concatenated string
+        funtion to concat str1 and str2 as
+        arguments and return a concatenated string
     """
     return str1 + str2
